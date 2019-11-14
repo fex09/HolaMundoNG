71b9e2ab4ef89a1c56d0e555f79e8bd38a744e25
Solís González Fenix <FeSoli@ice.go.cr>
2019-11-13 20:35:21 -0600

    username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HolaMundo';
+  public title = 'HolaMundo';
+  public username: string;
+
 }
-//Cambio asdfas fsd
